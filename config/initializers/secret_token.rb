5e84c78831646b1a151e39907c3820b11b9145a0
Vinicius Gomes <vvgomess@gmail.com>
2012-09-01 01:29:10 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaveJohnsonFoundation::Application.config.secret_token = '992d3527a0cb7303fb0d2e611250c282ace211859636ca0c88b6ee2a790d0815be86af56a5df25a773aac24be8ceb0020ab56105154f119821c9dc61a68a038e'
