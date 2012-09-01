2d89c734eb28e0cdbc6e2e2478ccb980fe47a939
Vinicius Gomes <vvgomess@gmail.com>
2012-09-01 06:13:41 -0400

    removing blank lines

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-
 apps = App.create([
   { name: 'Diaroogle', url: 'http://www.diaroogle.com' },
   { name: 'Twitter', url: 'http://www.twitter.com' },
