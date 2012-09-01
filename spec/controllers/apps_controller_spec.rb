51e049d68195f397bc0b087f1bd26e73b893175e
Vinicius Gomes <vvgomess@gmail.com>
2012-09-01 03:27:58 -0400

    controller#show

--- spec/controllers/apps_controller_spec.rb
+++ spec/controllers/apps_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe AppsController do
+  let(:diaroogle) { build :app }
+  let(:twitter) { build :app }
+
   describe '#index' do
-    let(:apps) {[
-      build(:app, name: 'Diaroogle'),
-      build(:app, name: 'Twitter')
-    ]}
+    let(:apps) { [diaroogle, twitter] }
 
     before do
       App.stub(:all).and_return apps
@@ -14,5 +14,15 @@ describe AppsController do
 
     it { assigns(:apps).should =~ apps }
     it { render_template :index }
+  end
+
+  describe '#show' do
+    before do
+      App.stub(:find).with('42').and_return diaroogle
+      get :show, id: 42
+    end
+
+    it { assigns(:app).should eq diaroogle }
+    it { render_template :show }
   end
 end
