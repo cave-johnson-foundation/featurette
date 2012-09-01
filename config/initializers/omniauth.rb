712b019c290f27de698b7102d223c7cb8165a5e7
Pedro Nascimento <pnascimento@gmail.com>
2012-09-01 18:57:39 -0300

    Disable ssl verification for development.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
-# -*- encoding : utf-8 -*-
+if Rails.env.development?
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+end
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], :scope => "email"
+  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], scope: "email", client_options: {:ssl => {:ca_path => "/etc/ssl/certs"}}
 end
