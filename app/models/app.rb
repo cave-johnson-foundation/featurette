6f68711b7f36f3d1757ccf13cfd75a91e66fcbd9
Joao Vortmann <jvortmann@gmail.com>
2012-09-02 15:13:41 -0300

    Making description accessible.

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,5 +1,5 @@
 class App < ActiveRecord::Base
-  attr_accessible :name, :url, :logo, :screenshot
+  attr_accessible :name, :description, :url, :logo, :screenshot
   has_many :features
   has_attached_file :logo, :styles => { :medium => "238x238>",:thumb => "100x100>"}
   has_attached_file :screenshot, :styles => { :medium => "238x238>",:thumb => "100x100>", :large => "500x500>"}
--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -1,5 +1,6 @@
 describe App do
   it { should respond_to :name }
+  it { should respond_to :description }
   it { should respond_to :url }
   it { should have_many :features }
   it {should have_attached_file :logo}
