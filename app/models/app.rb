e0a8a7724054038688ccc08b1ab8c31b1d896df0
Pedro Nascimento <pnascimento@gmail.com>
2012-09-02 06:04:09 -0300

    adding attached logo and screenshot.

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,6 @@
 class App < ActiveRecord::Base
-  attr_accessible :name, :url
+  attr_accessible :name, :url, :logo, :screenshot
   has_many :features
+  has_attached_file :logo, :styles => { :medium => "238x238>",:thumb => "100x100>"}
+  has_attached_file :screenshot, :styles => { :medium => "238x238>",:thumb => "100x100>", :large => "500x500>"}
 end
--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -2,4 +2,6 @@ describe App do
   it { should respond_to :name }
   it { should respond_to :url }
   it { should have_many :features }
+  it {should have_attached_file :logo}
+  it {should have_attached_file :screenshot}
 end
