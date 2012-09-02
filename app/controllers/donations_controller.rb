5defc98d33e01cf35047bec2344dca36e2bd18b4
Vinicius Gomes <vvgomess@gmail.com>
2012-09-02 14:58:53 -0400

    donations in new page

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,13 +1,25 @@
 class DonationsController < ApplicationController
+  def new
+    app = App.find params[:app_id]
+    feature = Feature.new(app: app)
+    @donation = Donation.new(feature: feature, user: current_user)
+  end
+
+  def show
+    @donation = Donation.find(params[:id])
+  end
+
   def create
     @feature = Feature.find(params[:feature_id])
-    @feature.receive Donation.new(params[:amount])
+    @donation = Donation.new(params[:amount])
+    @feature.receive @donation
+
     respond_to do |format|
-      if @feature.save
-        format.html { redirect_to app_feature_path(@feature.app, @feature), notice: 'Donation received!' }
+      if @donation.save && @feature.save
+        format.html { redirect_to app_feature_donation_path(@feature.app, @feature, @donation), notice: 'Donation received!' }
         format.json { head :no_content }
       else
-        format.html { render action: 'show', controller: 'features' }
+        format.html { render action: 'new', controller: 'donations' }
         format.json { render json: @feature.errors, status: :unprocessable_entity }
       end
     end
