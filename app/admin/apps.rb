baaf3ab0ff75ce08576cc2d88e9d0a6a86f4a339
Pedro Nascimento <pnascimento@gmail.com>
2012-09-02 06:03:50 -0300

    fixing form.

--- app/admin/apps.rb
+++ app/admin/apps.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register App do
-  
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :url
+      f.input :logo, :as => :file, :hint => f.template.image_tag(f.object.logo.url(:medium))
+      f.input :screenshot, :as => :file, :hint => f.template.image_tag(f.object.screenshot.url(:medium))
+    end
+    f.buttons
+  end
 end
