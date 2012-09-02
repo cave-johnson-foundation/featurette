b5947a583145499e17feef348cbe26e14cc50980
Pedro Nascimento <pnascimento@gmail.com>
2012-09-02 04:52:54 -0300

    specs for controller

--- spec/controllers/apps_controller_spec.rb
+++ spec/controllers/apps_controller_spec.rb
@@ -1,17 +1,22 @@
 describe AppsController do
   let(:diaroogle) { build :app }
   let(:twitter) { build :app }
+  let(:rich_one) { build :donation }
+  let(:poor_one) { build :donation }
 
   describe '#index' do
     let(:apps) { [diaroogle, twitter] }
+    let(:donations) { [rich_one, poor_one] }
 
     before do
       App.stub(:scoped).and_return apps
+      Donation.stub(:latest).and_return donations
       get :index
     end
 
     it { should respond_with(:success) }
     it { assigns(:apps).should =~ apps }
+    it { assigns(:latest_donations).should =~ donations }
     it { should render_template :index }
     it { should_not set_the_flash }
   end
