72c7f188373f45c2f8d21b25761a18b2ff32f809
Pedro Nascimento <pnascimento@gmail.com>
2012-09-02 05:48:24 -0300

    more active admin stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ CaveJohnsonFoundation::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  config.assets.precompile += %w[active_admin.css active_admin.js]
+  config.assets.precompile += %w[active_admin.css active_admin.js active_admin/print.css]
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
