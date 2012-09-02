5b0fd88dbb9bae914b4b1b7e1665b1598193df4c
Pedro Nascimento <pnascimento@gmail.com>
2012-09-02 05:34:00 -0300

    active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 CaveJohnsonFoundation::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   match "/auth/:provider/callback" => "sessions#create", :as => :auth_callback
   match "/auth/failure" => "sessions#failure", :as => :auth_failure
   match "/logout" => "sessions#destroy", :as => :logout
