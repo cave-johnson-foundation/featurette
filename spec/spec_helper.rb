c4995f612833bd6f7ba5db3c51fddef491224028
Pedro Nascimento <pnascimento@gmail.com>
2012-09-02 06:03:59 -0300

    adding paperclip matchers.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require "paperclip/matchers"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -36,5 +37,6 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
+  config.include Paperclip::Shoulda::Matchers
   config.include FactoryGirl::Syntax::Methods
 end
