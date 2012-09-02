9eaebfe6506438b382e25b597aef31acf54fd603
Pedro Nascimento <pnascimento@gmail.com>
2012-09-02 06:24:27 -0300

    adding description to active admin

--- app/admin/apps.rb
+++ app/admin/apps.rb
@@ -2,6 +2,7 @@ ActiveAdmin.register App do
   form :html => { :enctype => "multipart/form-data" } do |f|
     f.inputs "Details" do
       f.input :name
+      f.input :description
       f.input :url
       f.input :logo, :as => :file, :hint => f.template.image_tag(f.object.logo.url(:medium))
       f.input :screenshot, :as => :file, :hint => f.template.image_tag(f.object.screenshot.url(:medium))
